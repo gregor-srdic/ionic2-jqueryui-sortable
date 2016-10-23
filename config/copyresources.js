bdde8626e14dee524b95c1392c7050773283c984
Gregor Srdic <gregor.srdic@gmail.com>
2016-10-23 08:38:10 +0200

    copy lib resources

---
+++ config/copyresources.js
@@ -0,0 +1,24 @@
+module.exports = {
+  include: [
+    {
+      src: 'src/assets/',
+      dest: 'www/assets/'
+    },
+    {
+      src: 'src/index.html',
+      dest: 'www/index.html'
+    },
+    {
+      src: 'node_modules/ionic-angular/polyfills/polyfills.js',
+      dest: 'www/build/polyfills.js'
+    },
+    {
+      src: 'node_modules/ionicons/dist/fonts/',
+      dest: 'www/assets/fonts/'
+    },
+    {
+      src: 'src/lib/',
+      dest: 'www/lib/'
+    }
+  ]
+};
